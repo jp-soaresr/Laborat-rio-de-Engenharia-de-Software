213a024bd01f4ef317e9a2defd6ae8d48be37830
jp-soaresr <soaresr0105@gmail.com>
2025-02-11 20:12:12 -0300

    dia11

---
+++ atividades1/at3/ext3.js
@@ -0,0 +1,20 @@
+let numero = prompt("Digite um número inteiro positivo:");
+
+
+while (isNaN(numero) || numero <= 0) {
+  numero = prompt("Número inválido. Digite um número inteiro positivo:");
+
+}
+
+numero = parseInt(numero);
+
+
+var fatorial = 1;
+for(i= 1; i <= numero; i++){
+
+    fatorial *= i;  
+
+
+
+};
+alert('O resultado do seu numero e: ' +fatorial); 
